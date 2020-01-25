0b98283f47557f5df2d98b853d25f4220f67926c
ing-arriola <ingeniero.arriola@gmail.com>
2020-01-25 11:40:26 -0600

    Some conditional renderind added

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ var app= new Vue({
     el: '#app',//Name of the referenced element in the HTML
     data: {
         product: 'Diskman',//Data to be displayed
-        description: 'The best portable music player',
-        image: './images/diskman-sony.jpg' //Image ti be displayed
+        description: 'The best portable music player :)',
+        image: './images/diskman-sony.jpg', //Image ti be displayed
+        inventory:8,
+        sale:true
     }
 })
