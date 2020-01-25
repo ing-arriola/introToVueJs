d94c60711ee8ff15bae17b2d4efa23679f76461f
ing-arriola <ingeniero.arriola@gmail.com>
2020-01-25 11:27:08 -0600

    Added image of the product

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var app= new Vue({
     el: '#app',//Name of the referenced element in the HTML
     data: {
         product: 'Diskman',//Data to be displayed
-        description: 'The best portable music player'
+        description: 'The best portable music player',
+        image: './images/diskman-sony.jpg' //Image ti be displayed
     }
 })
