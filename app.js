610df23e670885b5f29129fce47b271d82a80d83
ing-arriola <ingeniero.arriola@gmail.com>
2020-01-25 16:19:23 -0600

    Added event to change product image

--- app.js
+++ app.js
@@ -3,20 +3,37 @@ var app= new Vue({
     data: {
         product: 'Diskman',//Data to be displayed
         description: 'The best portable music player :)',
-        image: './images/diskman-sony.jpg', //Image ti be displayed
+        image: './images/d-e225.jpg', //Image ti be displayed
         inventory:8,
         sale:true,
         details: ["Unlimited antishock","High sound quality","Long battery life","Rechargeable battery"],
         variants: [
             {
                 variantId:001,
-                variantBrand:"Sony"
+                variantBrand:"D-E225",
+                variantImage:"./images/d-e225.jpg"
             },
             {
                 variantId:002,
-                variantBrand:"Philips"
+                variantBrand:"D-E011",
+                variantImage:"./images/d-ej011.jpg"
             }
-        ]
+        ],
+        cart:0
+    },
+    methods:{
+        addToCart:function() {
+            this.cart+=1
+        },
+        updateProduct:function(variantImage){
+            this.image=variantImage
+        },
+        removeFromCart:function(){
+            if (this.cart>0) {
+                this.cart-=1    
+            }
+            
+        }
     }
     
 })
