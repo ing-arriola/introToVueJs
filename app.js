72695f1149c06c54f223ab6a76567d14c6d5294d
ing-arriola <ingeniero.arriola@gmail.com>
2020-01-25 10:16:32 -0600

    Added some styles

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var app= new Vue({
     el: '#app',//Name of the referenced element in the HTML
     data: {
-        product: 'Diskman'//Data to be displayed
+        product: 'Diskman',//Data to be displayed
+        description: 'The best portable music player'
     }
 })
