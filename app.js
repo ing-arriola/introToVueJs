f443f7741afd30d2c9353934f1fb77379b6c147d
ing-arriola <ingeniero.arriola@gmail.com>
2020-01-25 20:41:28 -0600

    Added computed properties

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ var app= new Vue({
     el: '#app',//Name of the referenced element in the HTML
     data: {
         product: 'Discman',//Data to be displayed
+        brand:'Sony',
         description: 'The best portable music player :)',
-        image: './images/d-e225.jpg', //Image ti be displayed
+        //image: './images/d-e225.jpg', //Image ti be displayed
+        selectedVariable:0,
         inventory:8,
         remove:false,
         sale:true,
@@ -13,16 +15,26 @@ var app= new Vue({
                 variantId:001,
                 variantBrand:"D-E225",
                 variantImage:"./images/d-e225.jpg",
-                variantColor:"grey"
+                variantColor:"grey",
+                variatnQuantity:10
             },
             {
                 variantId:002,
                 variantBrand:"D-E011",
                 variantImage:"./images/d-ej011.jpg",
-                variantColor:"black"
+                variantColor:"black",
+                variatnQuantity:0
             }
         ],
         cart:0
+    },
+    computed: {
+        title() {
+            return this.product + ' ' + this.brand
+        },
+        image(){
+            return this.variants[this.selectedVariable].variantImage
+        }
     },
     methods:{
         addToCart:function() {
@@ -31,8 +43,9 @@ var app= new Vue({
                 this.remove=false
             }
         },
-        updateProduct:function(variantImage){
-            this.image=variantImage
+        updateProduct:function(index){
+            this.selectedVariable=index
+            
         },
         removeFromCart:function(){
             if (this.cart>0) {
